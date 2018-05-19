ff979bc37325bfc8709b83bb5855895949471ffb
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-05-20 02:35:25 +0300

    add signing and verifying of binary data with pub/private key pairs.

--- __tests__/BadgeSignature.spec.ts
+++ __tests__/BadgeSignature.spec.ts
@@ -1,46 +1,118 @@
 import { Buffer } from 'buffer/';
 import BadgeSignature from '../src/BadgeSignature';
+import {ECDSACurve, ECDSASignatureFactory, HMACSignatureFactory} from "../src";
 
 describe('BadgeSignature spec', () => {
-    const __TEST_DATA__ = [
-        { data: Uint8Array.from([1]), signed: Uint8Array.from([8, 141, 22, 41, 119, 219, 63, 121, 243, 1]) },
-        { data: Uint8Array.from([1, 2, 3]), signed: Uint8Array.from([3, 172, 81, 211, 1, 2, 3]) },
-        { data: Buffer.from([1, 2, 3]), signed: Uint8Array.from([3, 172, 81, 211, 1, 2, 3]) }
-    ];
-    const bSignature = new BadgeSignature('test-signature');
+    const keys = {
+        ed25519: {
+            priv: new Buffer('DP5SS20yLZD6buZHX3VH4R6EXEcoNiFTQAvC8xdrfBQ=', 'base64'),
+            pub: new Buffer('BB7pV5ZPhJINorG3X3KRaZcDesYEgWtLHakUTMa9j7ryPJaAlK1dQeHPUDUZ0AQsJW0B1iCufJyVqnYlGP7nMs0=', 'base64'),
+        },
+        p192: {
+            priv: new Buffer('lKucBafgP8W2tWliZeaZEr7ckZrLdLuQ', 'base64'),
+            pub: new Buffer('BJqNJUck6GohkxQCXp0fz50umaViG9BSbmL7o8NlJfdboqYqeESaxCvfCaWwqmxSJg==', 'base64'),
+        },
+        secp256k1: {
+            priv: new Buffer('5O/RP7I+KJi3G1Xq/zs79+aZXSP8x0oWR0K6fngiru8=', 'base64'),
+            pub: new Buffer('BMGnGCIlCwgMrYaNV6+pmuPDbkKirlKMfi3qMH4V5q8X3c18EZfn7bSV8gt9t8m3fRZht0gB1vdd+bLAYvR/ZEM=', 'base64')
+        }
+    };
+
+    const ecdsaSignatureFactory = new ECDSASignatureFactory(ECDSACurve.ed25519, keys.ed25519.pub, keys.ed25519.priv);
+    const ecdsaSignature = new BadgeSignature(ecdsaSignatureFactory);
+    const ecdsap192SignatureFactory = new ECDSASignatureFactory(ECDSACurve.p192, keys.p192.pub, keys.p192.priv);
+    const ecdsap192Signature = new BadgeSignature(ecdsap192SignatureFactory);
+    const ecdsaSecp256k1SignatureFactory = new ECDSASignatureFactory(ECDSACurve.SECP256K1, keys.secp256k1.pub, keys.secp256k1.priv);
+    const ecdsaSecp256k1Signature = new BadgeSignature(ecdsaSecp256k1SignatureFactory);
+    const hmacSignatureFactory = new HMACSignatureFactory('test-signature');
+    const hmacSignature = new BadgeSignature(hmacSignatureFactory);
+
+    const __TEST_DATA__ = {
+        hmac: {
+            signature: hmacSignature,
+            tests:[
+                { data: Uint8Array.from([1]), signed: Uint8Array.from([8, 141, 22, 41, 119, 219, 63, 121, 243, 1]) },
+                { data: Uint8Array.from([1, 2, 3]), signed: Uint8Array.from([3, 172, 81, 211, 1, 2, 3]) },
+                { data: Buffer.from([1, 2, 3]), signed: Uint8Array.from([3, 172, 81, 211, 1, 2, 3]) }
+            ]
+        },
+        ecdsa: {
+            [ECDSACurve.ed25519]: {
+                signature: ecdsaSignature,
+                tests: [
+                    { data: Uint8Array.from([1, 2, 3]), signed: Uint8Array.from(new Buffer('QAIJExcCZQcnwH5OJxtDqHOID3YQ9mEnoNEz89iJGrh/DRflzfu8yL2TrsGo17zDLjb9/dmdGjyAqPIktOEOQy8BAgM=', 'base64')) },
+                    { data: Uint8Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]), signed: Uint8Array.from(new Buffer('QAvzyR1W78PLWSqz0S2FOyXvW2W3/ICHvnoK9BR2bSyoALtWfHidWkPdgQJbIM9y8maKPLv8oMJ25Dlue6Ijto0BAgMEBQYHCAkKCwwNDg8QERITFA==', 'base64')) }
+                ]
+            },
+            [ECDSACurve.p192]: {
+                signature: ecdsap192Signature,
+                tests: [
+                    { data: Uint8Array.from([1, 2, 3]), signed: Uint8Array.from(new Buffer('MIbzMtKeQkPvTn1mT5EiC6YgFEXRzmmJeqX6nJsW0r2vuONhA100RGua6zzvl+3FYwECAw==', 'base64')) },
+                    { data: Uint8Array.from([127, 207, 179, 168, 100, 125, 165, 204, 180, 178, 213, 36, 213, 69, 34, 50, 215, 32, 193, 151]), signed: Uint8Array.from(new Buffer('MOh9eBqKS9qdGbgETFssdxmFaTAVWJeqhApmTzi7XDmVH+ZtEWPN+r/ksbqNdi8YQH/Ps6hkfaXMtLLVJNVFIjLXIMGX', 'base64')) }
+                ]
+            },
+            [ECDSACurve.SECP256K1]: {
+                signature: ecdsaSecp256k1Signature,
+                tests: [
+                    { data: Uint8Array.from([1, 2, 3]), signed: Uint8Array.from(new Buffer('QPiTapn5xzZIBQ7UvMD5I6RfmKbbQ2iuhoj28txIBpPtl44po8ob4/v0wjpnQfozu4tRCX14xtT2Aa0NrBjPEp8BAgM=', 'base64')) },
+                    { data: Uint8Array.from([60, 35, 49, 135, 104, 231, 76, 107, 178, 4, 21, 36, 252, 99, 78, 180, 15, 161, 129, 93]), signed: Uint8Array.from(new Buffer('QMFkGAlJK0yZIOVpnKuM826GcgcTCvGDw92BYmMp+oL74QIffYV+k7zj7aTpE3qfIosHC9DxoHECUDESk/26e4Q8IzGHaOdMa7IEFST8Y060D6GBXQ==', 'base64')) },
+                ]
+            },
+        }
+    };
+    // @ts-ignore
+    const __TEST_DATA_MAPPED__ = [__TEST_DATA__.hmac, ...Object.values(__TEST_DATA__.ecdsa)];
 
     describe('signature creation', () => {
         it('should sign data', async () => {
-            for(let testCase of __TEST_DATA__){
-                const signed = await bSignature.sign(testCase.data, testCase.signed[0]);
-                expect(signed).toEqual(testCase.signed);
+            for(let testGroup of __TEST_DATA_MAPPED__) {
+                for (let testCase of testGroup.tests) {
+                    const signed = await testGroup.signature.sign(testCase.data, testCase.signed[0]);
+
+                    expect(signed).toEqual(testCase.signed);
+                }
             }
         });
 
-        it('should fail with invalid signature sizes', async () => {
+        it('hmac should fail with invalid signature sizes', async () => {
             const arr = Uint8Array.from([]);
 
-            expect(bSignature.sign(arr, -1)).rejects.toBeDefined();
-            expect(bSignature.sign(arr, 0)).rejects.toBeDefined();
-            expect(bSignature.sign(arr, 65)).rejects.toBeDefined();
+            expect(hmacSignature.sign(arr, -1)).rejects.toBeDefined();
+            expect(hmacSignature.sign(arr, 0)).rejects.toBeDefined();
+            expect(hmacSignature.sign(arr, 65)).rejects.toBeDefined();
+        });
+
+        it('hmac should truncate signatures properly', async () => {
+            const arr = Uint8Array.from([1]);
+
+            for (let i = 1; i < 65; i++) {
+                expect((await hmacSignature.sign(arr, i))[0]).toEqual(i);
+            }
+        });
+
+        it('ecdsa should ignore signature size', async () => {
+            const arr = Uint8Array.from([1, 2, 3]);
+            const signatureSize = 48;
+
+            for (let i = 1; i < 64; i++) {
+                expect((await ecdsap192Signature.sign(arr, i))[0]).toEqual(signatureSize);
+            }
         });
     });
 
     describe('verification of signatures', () => {
         it('should verify signatures', async () => {
-            for(let testCase of __TEST_DATA__){
-                const data = await bSignature.verify(testCase.signed);
-                expect(data).toEqual(Uint8Array.from(testCase.data));
+            for(let testGroup of __TEST_DATA_MAPPED__) {
+                for (let testCase of testGroup.tests) {
+                    const data = await testGroup.signature.verify(testCase.signed);
+                    expect(data).toEqual(Uint8Array.from(testCase.data));
+                }
             }
         });
 
-        it('should fail with invalid signatures', () => {
-            expect(bSignature.verify(Uint8Array.from([1]))).rejects.toBeDefined();
-            expect(bSignature.verify(Uint8Array.from([0]))).rejects.toBeDefined();
-        });
-
-        it('should accept zero length signatures', () => {
-            expect(bSignature.verify(Uint8Array.from([0, 1]))).resolves.toEqual(Uint8Array.from([1]));
+        it('hmac should fail with invalid signatures', async () => {
+            expect(hmacSignature.verify(Uint8Array.from([1]))).rejects.toBeDefined();
+            expect(hmacSignature.verify(Uint8Array.from([0]))).rejects.toBeDefined();
         });
     });
 });
--- src/BadgeSignature.ts
+++ src/BadgeSignature.ts
@@ -1,5 +1,7 @@
-import { HmacSHA512, lib, enc, LibWordArray } from 'crypto-js';
+import { HmacSHA512, MD5, lib, enc, LibWordArray } from 'crypto-js';
 import { wordArrayToUint8Array } from './utils';
+import { Buffer } from 'buffer/';
+import { ec as EC } from 'elliptic';
 
 /**
  * Given an uint8array and a secret, calculates and returns a truncated hmac-sha512 signature.
@@ -15,8 +17,119 @@ function hmacSignUint8Arr(bytes : Uint8Array, secret : string, bytesToReturn : n
     return wordArrayToUint8Array(sign, bytesToReturn);
 }
 
-export class BadgeSignature {
+function md5Hex(bytes: Uint8Array) : string {
+    return MD5(lib.WordArray.create(bytes)).toString(enc.Hex);
+}
+
+function uintToHex(uint8Arr: Uint8Array) : string {
+    return new Buffer(uint8Arr).toString('hex');
+}
+
+export interface SignatureFactory {
+    /**
+     * Sign the bytes and if possible truncate the signature.
+     * @param {Uint8Array} bytes
+     * @param {number} wantedSignatureLength
+     * @return {Uint8Array}
+     */
+    sign(bytes: Uint8Array, wantedSignatureLength: number) : Uint8Array
+    verify(bytes: Uint8Array, signature: Uint8Array) : boolean
+}
+
+export class HMACSignatureFactory implements SignatureFactory {
     constructor(private readonly signSecret: string){}
+
+    sign = (bytes: Uint8Array, bytesToReturn: number): Uint8Array => {
+        if(bytesToReturn < 1 || bytesToReturn > 64)
+            throw new Error('signature size should be between 1 and 64');
+        if(bytesToReturn === 0) return Uint8Array.from([]);
+
+        return hmacSignUint8Arr(bytes, this.signSecret, bytesToReturn);
+    };
+
+    verify = (bytes: Uint8Array, signature: Uint8Array): boolean => {
+        const calculated = this.sign(bytes, signature.length);
+        return calculated.toString() === signature.toString();
+    };
+}
+
+export enum ECDSACurve {
+    SECP256K1 = "secp256k1",
+    p192 = "p192",
+    p224 = "p224",
+    p256 = "p256",
+    p384 = "p384",
+    ed25519 = "ed25519",
+}
+
+function createKeyPair(ec: any) : { priv: Uint8Array, pub: Uint8Array } {
+    // @ts-ignore
+    const key = ec.genKeyPair({ entropy: wordArrayToUint8Array(lib.WordArray.random(32)) });
+
+    const priv = Uint8Array.from(key.getPrivate().toArray());
+    const pubKey = key.getPublic().encode('hex');
+    const pub = new Buffer(pubKey, 'hex');
+
+    return { priv, pub: Uint8Array.from(pub) };
+}
+
+const signaturePartSize = {
+    [ECDSACurve.SECP256K1]: 32,
+    [ECDSACurve.p192]: 24,
+    [ECDSACurve.p224]: 28,
+    [ECDSACurve.p256]: 32,
+    [ECDSACurve.p384]: 48,
+    [ECDSACurve.ed25519]: 32,
+};
+
+export class ECDSASignatureFactory implements SignatureFactory {
+    private ec : any;
+    private partSize: number;
+    private pubKey : any;
+    private privKey? : any;
+
+    constructor(
+        ecdsaCurve : ECDSACurve,
+        signPublic: Uint8Array,
+        signSecret?: Uint8Array,
+    ){
+        this.ec = new EC(ecdsaCurve.toString());
+        this.partSize = signaturePartSize[ecdsaCurve];
+        // @ts-ignore
+        this.pubKey = this.ec.keyFromPublic(uintToHex(signPublic), 'hex');
+        if(signSecret)
+            this.privKey = this.ec.keyFromPrivate(uintToHex(signSecret), 'hex');
+    }
+
+    sign(bytes: Uint8Array, wantedSignatureLength: number): Uint8Array {
+        if(!this.privKey)
+            throw new Error('can not sign without secret key when using ecdsa');
+        const hash = md5Hex(bytes);
+
+        const signature = this.privKey.sign(hash);
+
+        return Uint8Array.from(
+            signature.r.toArray('be', this.partSize)
+                .concat(signature.s.toArray('be', this.partSize))
+        );
+    }
+
+    verify(bytes: Uint8Array, signature: Uint8Array): boolean {
+        const sigHex = uintToHex(signature);
+        const halfSignature = sigHex.length / 2;
+        const r = sigHex.substring(0, halfSignature);
+        const s = sigHex.substring(halfSignature);
+        const hash = md5Hex(bytes);
+
+        return this.pubKey.verify(hash, { r, s });
+    }
+}
+
+
+export class BadgeSignature {
+    //private signatureFactory : SignatureFactory;
+
+    constructor(private readonly signatureFactory: SignatureFactory){}
 
     /**
      * Signs the given uint8arr with the instance's secret.
@@ -26,9 +139,7 @@ export class BadgeSignature {
      * @return {Promise<Badge>}
      */
     sign = async (uint8Arr: Uint8Array, bytesToStore: number = 64) : Promise<Uint8Array> => {
-        if(bytesToStore < 1 || bytesToStore > 64)
-            throw new Error('signature size should be between 1 and 64');
-        const sign = hmacSignUint8Arr(uint8Arr, this.signSecret, bytesToStore);
+        const sign = this.signatureFactory.sign(uint8Arr, bytesToStore);
 
         const result = new Uint8Array(1 + sign.length + uint8Arr.length);
         result.set([sign.length], 0);
@@ -47,10 +158,10 @@ export class BadgeSignature {
         // there needs to be at least 1 byte of data after signature.
         if(signatureLength >= signedBytes.length - 1)
             throw new Error('too big of a signature length for the given array.');
+        const signature = signedBytes.slice(1, signatureLength + 1);
         const badgeBytes = signedBytes.slice(signatureLength + 1);
-        const sign = hmacSignUint8Arr(badgeBytes, this.signSecret, signatureLength);
 
-        if(sign.toString() !== signedBytes.slice(1, signatureLength + 1).toString())
+        if(this.signatureFactory.verify(badgeBytes, signature) !== true)
             throw new Error('could not verify signatures. the signature does not match the stored badge.');
         return Uint8Array.from(badgeBytes);
     };
